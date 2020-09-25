bfbcbde4c966fea330e1fac5c73bd418f686d51e
calvia76 <65033344+calvia76@users.noreply.github.com>
2020-09-25 19:33:41 -0400

    SLINGSHOT SCRIPT
    -Created Slingshot script
    -Added mouse detection to Slingshot object for future functionality

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//SetActive method on GameObj tells game whether or not to ignore them
+
+public class Slingshot : MonoBehaviour
+{
+    public GameObject launchPoint;
+
+    private void Awake()
+    {   //transform.Find searches for a child of Slingshot named LP and returns
+        //its transform. 
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        //gets gameobj associated with Transform and assigns to game obj field 'launchPoint'
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+
+    }
+    private void OnMouseEnter()
+    {
+
+        //print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
